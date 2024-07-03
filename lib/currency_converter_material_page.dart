243a9b35c403d297b3e6e921aabb8358f192867b
Hansika <nostochk@gmail.com>
2024-07-03 18:29:43 +0530

    create a CurrencyConverterMaterialPage

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget{
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello Flutter & Dart ! 😎"),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:currency_converter/currency_converter_material_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-//Types of widgets
-//1. State less Widget
-//2. State ful Widget
-//3. Inherited Widget
-
-//State
-// refers to data that will determine how a widget looks like
-
-//Material design
-//cupertino design
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -21,11 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello Flutter & Dart ! 😎"),
-        ),
-      ),
+      home: CurrencyConverterMaterialPage(),
     );
   }
 }
