351eaca6cfc97a4b1851098e2dbfef06ea18053b
Hansika <nostochk@gmail.com>
2024-07-06 21:18:10 +0530

    add text widget

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
-class CurrencyConverterMaterialPage extends StatelessWidget{
+class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello Flutter & Dart ! 😎"),
+    return const Scaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "0",
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromRGBO(24, 119, 242, 1),
+              ),
+            ),
+            TextField(
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              decoration: InputDecoration(
+                hintText: "Please enter the amount in USD",
+                hintStyle: TextStyle(
+                  fontSize: 20,
+                  color: Color.fromRGBO(112, 112, 112, 1),
+                ),
+                suffixIcon: Icon(Icons.monetization_on_rounded),
+                suffixIconColor: Color.fromRGBO(112, 112, 112, 1),
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:currency_converter/currency_converter_material_page.dart';
+import 'package:flutter/material.dart';
+
 
 void main() {
   runApp(const MyApp());
